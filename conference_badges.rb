a01f7a46df9bffba9b80c9fd118f5f848dfd0766
Catherine Price <catprice444@gmail.com>
2020-04-22 20:39:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ badges = []
 end
 
 def assign_rooms(attendees)
-  batch_badge_creator(attendees)
+  room_assignments = []
   attendees.each_with_index do |name, index|
     puts "Hello #{name}! You'll be assigned to room #{index}!"
   end
