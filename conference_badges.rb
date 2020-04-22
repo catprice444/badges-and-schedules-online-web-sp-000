4c119e6693e580fa451b969755fb9266428ce7e0
Catherine Price <catprice444@gmail.com>
2020-04-22 19:56:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |person|
-     puts "Hello, my name is #{person}."
+  attendees.each do |name|
+     badge_maker(name)
   end
 end
